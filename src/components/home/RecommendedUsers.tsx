b0dd7133e692419e0608d3849af8744211f679fe
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-05-14 15:28:18 +0330

    refactor recommended user with backend data

--- src/components/home/RecommendedUsers.tsx
+++ src/components/home/RecommendedUsers.tsx
@@ -1,75 +1,15 @@
-import { useState } from "react";
 import Avatar from "../Ui/Avatar";
 import avatar from "../../assets/avatar.png";
-import type { RecommendedUser } from "../../types/RecommendedUser";
+import { useGetRecommendedUsers } from "../../hooks/useGetRecommendedUsers";
 
-const initialUsers: RecommendedUser[] = [
-  {
-    id: "MSg1gAmYplPDsyG1Bgk3jGsBI483cbPe",
-    name: "hadi",
-    email: "hadi@gmail.com",
-    emailVerified: false,
-    image: null,
-    bio: null,
-    location: null,
-    website: null,
-    createdAt: "2026-02-25T10:59:34.665Z",
-    updatedAt: "2026-02-25T10:59:34.665Z",
-    _count: { followers: 0 },
-    isFollowing: false,
-  },
-  {
-    id: "auRGViZsMXLr00Jy6aq5l7K8rsh9SSpQ",
-    name: "nastaran",
-    email: "nastaranadib1383@gmail.com",
-    emailVerified: false,
-    image: null,
-    bio: null,
-    location: null,
-    website: null,
-    createdAt: "2026-02-25T07:37:07.074Z",
-    updatedAt: "2026-02-25T07:37:07.074Z",
-    _count: { followers: 0 },
-    isFollowing: false,
-  },
-  {
-    id: "42pLF9HtxQHoRmRytrrYYp7VHp7pcnsR",
-    name: "morez",
-    email: "morez@gmail.com",
-    emailVerified: false,
-    image: null,
-    bio: null,
-    location: null,
-    website: null,
-    createdAt: "2026-02-24T18:11:34.422Z",
-    updatedAt: "2026-02-25T18:11:34.422Z",
-    _count: { followers: 1 },
-    isFollowing: false,
-  },
-];
 
   const RecommendedUsers = () => {
-    const [users, setUsers] = useState<RecommendedUser[]>(initialUsers);
 
+    const {data : recommendUsers} = useGetRecommendedUsers()
+    
     const handleToggleFollow = (userId: string) => {
-      setUsers((prevUsers) =>
-        prevUsers.map((user) => {
-          if (user.id === userId) {
-            const isCurrentlyFollowing = user.isFollowing;
-
-            return {
-              ...user,
-              isFollowing: !isCurrentlyFollowing,
-              _count: {
-                followers: isCurrentlyFollowing
-                  ? user._count.followers - 1
-                  : user._count.followers + 1,
-              },
-            };
-          }
-          return user;
-        }),
-      );
+      console.log(userId);
+      
     };
 
     return (
@@ -78,7 +18,7 @@ const initialUsers: RecommendedUser[] = [
           <h3 className="text-lg font-bold mb-6 dark:text-white">
             Recommended users
           </h3>
-          {users.map((user) => (
+          {recommendUsers?.map((user) => (
             <div
               key={user.id}
               className="flex items-center justify-between gap-x-30 mb-3"

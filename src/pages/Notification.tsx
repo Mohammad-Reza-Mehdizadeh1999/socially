58211864ee646c598cfdc554c22265d2dde0c575
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-05-25 06:22:53 +0330

    refactore dark light and responsive in notif page

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,23 +1,35 @@
 import Avatar from "../components/Ui/Avatar";
 import avatar from "../assets/avatar.png";
 import { useGetAllNotifications } from "../hooks/useGetAllNotification";
-import { useMarkOneAsRead, useMarkAllAsRead } from "../hooks/useMarkNotificationsRead";
+import {
+  useMarkOneAsRead,
+  useMarkAllAsRead,
+} from "../hooks/useMarkNotificationsRead";
 import toast from "react-hot-toast";
 import NotFoundPage from "./NotFoundPage";
 import { useAuthStore } from "../store/authStore";
 import { getTimeAgo } from "../utiles/geTimeAgo";
-import type { Notification, NotificationType } from "../types/NotificationTypes";
+import type {
+  Notification,
+  NotificationType,
+} from "../types/NotificationTypes";
 import Button from "../components/Ui/Button";
 
 const NotificationsPage = () => {
-
-  const { data: notificationsData, isLoading, isError, error} = useGetAllNotifications();
+  const {
+    data: notificationsData,
+    isLoading,
+    isError,
+    error,
+  } = useGetAllNotifications();
   const { isAuthenticated } = useAuthStore();
 
   const markOneMutation = useMarkOneAsRead();
   const markAllMutation = useMarkAllAsRead();
 
-  const unreadCount = notificationsData ? notificationsData.filter((notif) => !notif.read).length : 0;
+  const unreadCount = notificationsData
+    ? notificationsData.filter((notif) => !notif.read).length
+    : 0;
 
   if (!isAuthenticated) {
     return <NotFoundPage />;
@@ -100,7 +112,6 @@ const NotificationsPage = () => {
   };
 
   const handleMarkAllRead = () => {
-
     const notReadNotifIds: string[] = [];
 
     notificationsData?.forEach((notif) => {
@@ -108,11 +119,11 @@ const NotificationsPage = () => {
         notReadNotifIds.push(notif.id);
       }
     });
-    
+
     if (notReadNotifIds.length > 0) {
       markAllMutation.mutate(notReadNotifIds);
-    } else{
-      toast.success("You’ve read all notifications")
+    } else {
+      toast.success("You’ve read all notifications");
     }
   };
 
@@ -137,26 +148,41 @@ const NotificationsPage = () => {
   }
 
   return (
-    <section className="px-5">
-      <div className="mt-6 flex flex-col mx-auto max-w-154 lg:max-w-190 xl:max-w-244 h-140 border border-border-light dark:border-border-dark dark:bg-primary-light rounded-xl">
-        <div className="flex justify-between items-center p-6 sticky">
-          <h4 className="text-base font-bold leading-4 dark:text-white">
-            Notifications
-          </h4>
-          <div className="flex items-center justify-center gap-2">
-            <span className="text-sm leading-5 text-secondery-light dark:text-secondary-dark">
+    <section className="px-4 sm:px-5">
+      <div
+        className="
+      mt-6 flex flex-col mx-auto 
+      max-w-full sm:max-w-154 lg:max-w-190 xl:max-w-244 
+      h-140 
+      border border-border-light dark:border-border-dark 
+      bg-white dark:bg-primary-light 
+      rounded-xl
+    "
+      >
+        <div className="flex justify-between items-center p-4 sm:p-6 sticky top-0 bg-white dark:bg-primary-light z-10">
+          <h4 className="text-base font-bold dark:text-white">Notifications</h4>
+
+          <div className="flex items-center gap-2">
+            <span className="text-sm text-secondery-light dark:text-secondary-dark">
               {unreadCount} unread
             </span>
+
             <Button
               onClick={handleMarkAllRead}
-              className="text-sm font-medium hover:bg-gray-100 dark:hover:bg-border-dark px-3 py-1 cursor-pointer transition-colors rounded-md dark:text-white"
+              className="
+            text-sm font-medium px-3 py-1 rounded-md cursor-pointer transition-colors
+            hover:bg-gray-100 dark:hover:bg-border-dark
+            dark:text-white
+          "
             >
               {markAllMutation.isPending ? "Marking..." : "Mark as read"}
             </Button>
           </div>
         </div>
+
         <span className="block h-px w-full bg-border-light dark:bg-border-dark"></span>
-        <div className="flex flex-col space-y-3 max-h-full p-4 overflow-y-auto"> 
+
+        <div className="flex flex-col space-y-3 max-h-full p-3 sm:p-4 overflow-y-auto">
           {notificationsData?.map((notif) => (
             <div
               key={notif.id}
@@ -165,36 +191,57 @@ const NotificationsPage = () => {
                   markOneMutation.mutate(notif.id);
                 }
               }}
-              className={`flex items-start relative p-2 gap-3 bg-gray-100 dark:bg-border-dark rounded-md border border-border-light dark:border-border-dark ${
-                notif.read
-                  ? "bg-white dark:bg-primary-light"
-                  : "bg-gray-100 dark:bg-border-dark cursor-pointer"
-              }`}
+              className={`
+            flex items-start relative p-2 gap-3 
+            rounded-md border 
+            border-border-light dark:border-border-dark
+            transition-colors
+
+            ${
+              notif.read
+                ? "bg-white dark:bg-primary-light"
+                : "bg-gray-100 dark:bg-border-dark cursor-pointer"
+            }
+          `}
             >
-              <Avatar src={avatar} height={30} width={30}></Avatar>
+              <Avatar src={avatar} height={30} width={30} />
+
               <div className="flex flex-col gap-2 w-full">
-                <div className="flex items-center gap-2 w-full">
+                <div className="flex items-center gap-2 w-full flex-wrap">
                   {renderIcon(notif.type)}
+
                   <span className="text-base font-medium dark:text-white">
                     {notif.creator.name}
                   </span>
-                  <span className="text-sm leading-5 text-secondery-light dark:text-secondary-dark">
+
+                  <span className="text-sm text-secondery-light dark:text-secondary-dark">
                     {getNotificationText(notif)}
                   </span>
                 </div>
+
                 {notif.post && (
                   <span className="dark:text-white">{notif.post.content}</span>
                 )}
+
                 {notif.comment && (
-                  <span className="dark:text-white w-1/2 border my-2 p-3 rounded-lg dark:border-gray-600">
+                  <span
+                    className="
+                  dark:text-white 
+                  w-full sm:w-1/2 
+                  border my-2 p-3 rounded-lg 
+                  border-gray-300 dark:border-gray-600
+                "
+                  >
                     {notif.comment.content}
                   </span>
                 )}
-                <span className="text-sm leading-5 text-secondery-light dark:text-secondary-dark">
+
+                <span className="text-sm text-secondery-light dark:text-secondary-dark">
                   {getTimeAgo(notif.createdAt)}
                 </span>
               </div>
-              {notif.read || (
+
+              {!notif.read && (
                 <span className="size-2 rounded-full absolute bg-blue-500 right-4 top-4"></span>
               )}
             </div>

4e66d79a6189c737e6f7c0b344e330831605f63a
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-05-26 06:14:36 +0330

    added theme in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  document.documentElement.setAttribute('data-theme', savedTheme);
+}
+
 createRoot(document.getElementById("root")!).render(
   <>
     <Toaster position="top-center" />

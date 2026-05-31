cc7b6fc74c5df6325fb225edb8aa00c1ebfcb2aa
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-05-31 18:28:00 +0330

    added validation for recommended users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import AllPosts from "../components/home/AllPosts";
 import RecommendedUsers from "../components/home/RecommendedUsers";
+import { useAuthStore } from "../store/authStore";
 
 export default function Home() {
+
+  const { isAuthenticated } = useAuthStore()
+
   return (
     <div className="flex gap-5">
       <AllPosts />
-      <RecommendedUsers />
+      { isAuthenticated && <RecommendedUsers />}
     </div>
   )
 }

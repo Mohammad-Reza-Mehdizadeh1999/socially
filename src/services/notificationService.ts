a3fef8aa7577730d178ff5b87beac6a84472ae6f
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-06-01 20:43:28 +0330

    added service fn for mark one notif read

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -4,3 +4,8 @@ import api from "./axiosConfig";
 export const getAllNotificationsRequest = () => {
   return api.get("/notifications");
 };
+
+export const markOneNotificationRead = (ids: string) => {
+  const idToTransfer = { ids : [ids] }  
+  return api.patch(`/notifications` , idToTransfer);
+};
